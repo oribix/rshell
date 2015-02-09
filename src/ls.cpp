d59f1600760515f615e78d51d814e682d97b299b
Marco Morelos <mmore019@hammer.cs.ucr.edu>
2015-02-09 05:52:06 -0800

    fixed the number after the permissions, before the username. As it turns out, that is not the inode number but rather the number of hardlinks

--- src/ls.cpp
+++ src/ls.cpp
@@ -3,7 +3,7 @@
 #include <iomanip>
 #include <ctime>
 #include <cstring>
-#include <algorithm>
+//#include <algorithm>
 #include <queue>
 #include <list>
 #include <cstdlib>
@@ -17,9 +17,11 @@
 
 using namespace std;
 
+void printcontents(char* dirName, bool flaga, bool flagl);
+
 int main(int argc, char *argv[])
 {
-	char defdir[] = ".";//default directory
+	char defdir[] = "./";//default directory
 	char *dirName = defdir;//lets me change the directory
 	
 	bool flaga = false;
@@ -84,8 +86,11 @@ int main(int argc, char *argv[])
 				exit(EXIT_FAILURE);
 			}
 		}
-	}
+	}//end parsing parameters
 	
+	printcontents(dirName, flaga, flagl);
+	
+	/*
 	DIR *dirp = opendir(dirName);
 	if(dirp == NULL) {
 		perror("opendir");
@@ -105,17 +110,10 @@ int main(int argc, char *argv[])
 			filenames.push_back(string(direntp->d_name));
 			//cout << direntp->d_name << endl;  // use stat here
 		}
-		
 	}
 	
 	filenames.sort();//strict weak sort of the filenames
 	
-	//queue<int> longdetails;
-	//for (list<string> iterator it = filenames.begin() ; it != filenames.end() ; it++) {
-		//struct stat s;
-		//stat(*it, &s);
-		//longdetails.push_back(s);
-	//}
 	
 	while (filenames.size() != 0) {
 		if(flagl) {
@@ -158,6 +156,98 @@ int main(int argc, char *argv[])
 			}
 			
 			cout << setw(inodewidth) << right << s.st_ino << " ";
+			cout << setw(userwidth) << left << username->pw_name << " ";
+			cout << setw(groupwidth) << left << groupname->gr_name << " ";
+			cout << setw(bytewidth) << right << s.st_size << " ";
+			//output month day hour:minute
+			for(int i = 4 ; i < 16 ; i++) cout << timestring[i];
+			cout << " " << flush;
+			
+		}
+		
+		cout << filenames.front() << flush;
+		
+		if(flagl) cout << endl;
+		else cout << " " << flush;
+		
+		filenames.pop_front();
+	} 
+	if (!flagl) cout << endl;
+	
+	if (closedir(dirp) == -1) {
+		perror("closedir");
+		exit(EXIT_FAILURE);
+	}*/
+}
+
+void printcontents(char* dirName, bool flaga, bool flagl) {
+	DIR *dirp = opendir(dirName);
+	if(dirp == NULL) {
+		perror("opendir");
+		exit(EXIT_FAILURE);
+	}
+
+	dirent *direntp;
+	
+	list<string> filenames;//queue of filenames so that we can sort alphabetically 
+	
+	while ((direntp = readdir(dirp))) {
+		if(direntp == NULL) {
+			perror("readdir");
+			exit(EXIT_FAILURE);
+		}
+		if(flaga || (direntp->d_name)[0] != '.') {//checks for -a
+			filenames.push_back(string(direntp->d_name));
+			//cout << direntp->d_name << endl;  // use stat here
+		}
+	}
+	
+	filenames.sort();//strict weak sort of the filenames
+	
+	
+	while (filenames.size() != 0) {
+		if(flagl) {
+			//cerr << "files: " << filenames.front() << endl;
+			struct stat s;
+			string file = string(dirName) + filenames.front();
+			if( -1 == stat(file.c_str(), &s)) {
+				perror("stat");
+				exit(EXIT_FAILURE);
+			}
+			
+			//takes up 10 spaces
+			if(s.st_mode & S_IFDIR) cout << "d";
+			else cout << "-";
+			cout << ((s.st_mode&S_IRUSR)?"r":"-");
+			cout << ((s.st_mode&S_IWUSR)?"w":"-");
+			cout << ((s.st_mode&S_IXUSR)?"x":"-");
+			cout << ((s.st_mode&S_IXGRP)?"r":"-");
+			cout << ((s.st_mode&S_IXGRP)?"w":"-");
+			cout << ((s.st_mode&S_IXGRP)?"x":"-");
+			cout << ((s.st_mode&S_IXOTH)?"r":"-");
+			cout << ((s.st_mode&S_IXOTH)?"w":"-");
+			cout << ((s.st_mode&S_IXOTH)?"x":"-") << " " << flush;
+			
+			int inodewidth = 10;
+			int userwidth = 10;
+			int groupwidth = 10;
+			int bytewidth = 9;
+			char *timestring = ctime(&s.st_mtime);
+			//time_t *currenttime = time(NULL);
+			
+			struct passwd *username = getpwuid(s.st_uid);
+			if(username == NULL) {
+				perror("getpwuid");
+				exit(EXIT_FAILURE);
+			}
+			
+			struct group *groupname = getgrgid(s.st_gid);
+			if(groupname == NULL) {
+				perror("getpwgid");
+				exit(EXIT_FAILURE);
+			}
+			
+			cout << setw(inodewidth) << right << s.st_nlink << " ";
 			cout << setw(userwidth) << left << username->pw_name << " ";
 			cout << setw(groupwidth) << left << groupname->gr_name << " ";
 			cout << setw(bytewidth) << right << s.st_size << " ";
