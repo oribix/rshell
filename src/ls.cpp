f30c8558ee7956846d0b5509859840e51fb2188d
Marco Morelos <mmore019@hammer.cs.ucr.edu>
2015-02-08 16:54:42 -0800

    added -a functionality to ls. -all not implemented

--- src/ls.cpp
+++ src/ls.cpp
@@ -7,15 +7,48 @@
 
 using namespace std;
 
-/*
- *  * This is a BARE BONES example of how to use opendir/readdir/closedir.
- *   * Notice that there is no error checking on these functions.
- *    * You MUST add error checking yourself.
- *     */
-
-int main()
+int main(int argc, char *argv[])
 {
-	char dirName[] = ".";
+	char dirName[] = ".";//default directory
+	
+	bool flaga = false;
+	//bool flagl = false;
+	//bool flagR = false;
+
+	//parsing parameters
+	for (int i = 0 ; i < argc ; i++) {
+		//I might need this. I dont know yet
+		//int arglen;
+		//for (arglen = 0 ; argv[i][arglen] != '\0' ; arglen++)
+
+		if(argv[i][0] == '\0') { //should never trigger. It's here for protection
+			cerr << "error: empty argument found" << endl;
+		}
+		else if(argv[i][0] == '-') {
+			if(argv[i][1] == '\0') {
+				//FIXME: Print a GOOD error message
+				cerr << "error: '-' has no parameters"
+					<< endl;
+				exit(EXIT_FAILURE);
+			}
+			else if(argv[i][1] == '-') {
+				//check -all -long -recursive
+				
+			}
+			else {
+				//check -a -l -R
+				for(int j = 0 ; argv[i][j] != '\0' ; j++) {
+					if (argv[i][j] == 'a') flaga = true;
+					//if (argv[i][j] == 'l') flagl = true;
+					//if (argv[i][j] == 'R') flagR = true;
+				}
+			}
+		}
+		else {
+			//take path
+		}
+	}
+	
 	DIR *dirp = opendir(dirName);
 	if(dirp == NULL) {
 		perror("opendir");
@@ -28,7 +61,9 @@ int main()
 			perror("readdir");
 			exit(EXIT_FAILURE);
 		}
-		cout << direntp->d_name << endl;  // use stat here
+		if(flaga || (direntp->d_name)[0] != '.') {
+			cout << direntp->d_name << endl;  // use stat here
+		}
 	}
 	if (closedir(dirp) == -1) {
 		perror("closedir");
